7b52bef83967f9333fcf0a4b81261f1d15b48bde
mJehanno <mathob.jehanno@hotmail.fr>
2026-06-22 13:13:09 +0200

    refactor(model): expose model by moving it from internal to pkg

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 
 	tea "charm.land/bubbletea/v2"
 	"charm.land/lipgloss/v2"
-	"github.com/mJehanno/bubble-tab/internal/model"
+	"github.com/mJehanno/bubble-tab/pkg/model"
 )
 
 type Theme struct {
